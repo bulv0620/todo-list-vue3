c62c76d11a9d336742365f4ea70fa3db1f14acd7
bulv0620 <1241278815@qq.com>
2022-06-10 21:30:35 +0800

    Simple version

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <TodoList></TodoList>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
